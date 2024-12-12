0863e611e5a66ecf3fe7b206495ea4e69c3f8698
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2024-12-12 22:15:41 +0330

    create new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_tutorial';
+  title = `Angular ${new Date().getFullYear()}`;
+
 }
